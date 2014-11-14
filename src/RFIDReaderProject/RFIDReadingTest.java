bdd36a07bdf0cc50ed0a12f099737c24089248d5
dejuan <dejuan@mit.edu>
2014-11-14 16:44:48 -0500

    Added logic to automatically compute phase differences for tags detected more than once on a two antenna system. Will print to System.err, same as before.

--- src/RFIDReaderProject/RFIDReadingTest.java
+++ src/RFIDReaderProject/RFIDReadingTest.java
@@ -1,5 +1,12 @@
 package RFIDReaderProject;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.thingmagic.ReadPlan;
 import com.thingmagic.Reader;
 import com.thingmagic.ReaderException;
@@ -54,19 +61,51 @@ public class RFIDReadingTest {
 		try {
 			RFIDReadingTest reader = new RFIDReadingTest("DeJuan");
 			TagReadData[] readings = reader.readTags();
+			Map<String, Double> radianStorage = new HashMap<String, Double>();
 			if (readings.length == 0){
 				System.err.println("No tags were detected.");
 			}
 			else{
+				//Current implementation works for exactly two readings, not more; you need to do averaging/other work in that case.
+				List<String> doubleDetectedTags = new ArrayList<String>();
+				for (int i = 0; i < readings.length; i++){
+					String currentTag = readings[i].epcString();
+					double radians = readings[i].getPhase()*Math.PI;
+					radians = radians/180.0;
+					if(!radianStorage.containsKey(currentTag)){
+						radianStorage.put(currentTag, radians);
+					}
+					else{
+						double overlapDistance = Math.PI - Math.max(radianStorage.get(currentTag), radians);
+						overlapDistance += Math.min(radianStorage.get(currentTag), radians);
+						System.err.printf("Currently, radianStorage has the value %f for the tag %s." + System.getProperty("line.separator"), radianStorage.get(currentTag), currentTag);
+						radianStorage.put(currentTag, Math.min(Math.abs(radianStorage.get(currentTag)-radians), overlapDistance));
+						System.err.printf("After comparison, radianStorage has the value %f for the tag %s." + System.getProperty("line.separator"), radianStorage.get(currentTag), currentTag);
+						doubleDetectedTags.add(currentTag);
+					}
+					
+				}
 				System.err.println("Note: The higher the RSSI, the stronger the received signal is. Watch for negatives!");
 				System.err.println("");
 				for (int i = 0; i < readings.length; i++){
+					double radians = readings[i].getPhase()*Math.PI;
+					radians = radians/180.0;
 					System.err.printf("Tag %s was read: "
 							+ "This tag is number %d in the order of detected tags."
 							+ System.getProperty("line.separator") 
 							+ "The above tag has frequency %d and phase %d, with Received Signal Strength Indication(RSSI) of %d."
 							+ System.getProperty("line.separator")
-							+ System.getProperty("line.separator"), readings[i].toString(), i, readings[i].getFrequency(), readings[i].getPhase(), readings[i].getRssi());
+							+ "In radians, the phase is %f."
+							+ System.getProperty("line.separator")
+							+ System.getProperty("line.separator"), readings[i].toString(), i, readings[i].getFrequency(), 
+							readings[i].getPhase(), readings[i].getRssi(), radians);
+					
+				}
+				for(String tag : radianStorage.keySet()){
+					if (!doubleDetectedTags.contains(tag)){
+						continue;
+					}
+					System.err.printf("For the tag %s, the phase difference detected between the two antenna readings is %f", tag, radianStorage.get(tag));
 				}
 			}
 
