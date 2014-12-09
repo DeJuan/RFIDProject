4e08b2ea4d480811692c45ecd7310afc29ab43bc
dejuan <dejuan@mit.edu>
2014-12-09 15:06:30 -0500

    Commented most of the esoteric lines and javadoced the helper method and listener implementation.

--- src/RFIDReaderProject/RFIDContinuousReading.java
+++ src/RFIDReaderProject/RFIDContinuousReading.java
@@ -48,6 +48,12 @@ public class RFIDContinuousReading{
 		return readTags;
 	}
 	
+	/**
+	 * Given a TagReadData, this method will give you the FIRST reading from the three antennas of that tag.
+	 * If the tag wasn't detected, you'll just get an empty list back. 
+	 * @param tag : TagReadData object whose info you want from all three antennas
+	 * @return List<TagReadData> : the first reading from each of the three antennas of this tag
+	 */
 	public List<TagReadData> extractGivenTag(TagReadData tag){
 		List<TagReadData> reads = new ArrayList<TagReadData>();
 		if(antennaOneData.contains(tag)){
@@ -64,20 +70,21 @@ public class RFIDContinuousReading{
 	
 	public static void main(String args[]) throws InterruptedException{
 		try {
-			RFIDReadingTest reader = new RFIDReadingTest("Cynthia");
-			int totalTimeToRead = 2000;
-			int aSyncOnTime = 250;
-			int aSyncOffTime = 150;
-			reader.reader.paramSet("reader/read/asyncOnTime", aSyncOnTime);
+			RFIDReadingTest reader = new RFIDReadingTest("Cynthia"); //Initialize to your settings
+			int totalTimeToRead = 2000; 
+			int aSyncOnTime = 250; //total time to read before taking a break
+			int aSyncOffTime = 150; //how long to take a break for; so this reads "Read for .250 seconds, then break for .150 seconds, then repeat until 2 seconds have passed.
+			reader.reader.paramSet("reader/read/asyncOnTime", aSyncOnTime); //setting the reader up to know to do that
 			reader.reader.paramSet("reader/read/asyncOffTime", aSyncOffTime);
-			SeperateRecordsListener srl = new SeperateRecordsListener();
-			reader.reader.addReadListener(srl);
-			reader.reader.startReading();
-			Thread.sleep(totalTimeToRead);
-			reader.reader.stopReading();
-			Map<String, List<TagReadData>> allReadData = srl.returnDataCollected();
-			reader.reader.removeReadListener(srl);
-			for(String key : allReadData.keySet()){
+			SeperateRecordsListener srl = new SeperateRecordsListener(); //initialize Listener
+			reader.reader.addReadListener(srl); //attach listener to reader so it'll get the readTag notifications. See the API if this bit's confusing.
+			reader.reader.startReading(); //start the reader 
+			Thread.sleep(totalTimeToRead); //let other work go on; this will let the reader keep working in the background for the allotted time period.
+			reader.reader.stopReading(); //Stop the reader 
+			Map<String, List<TagReadData>> allReadData = srl.returnDataCollected(); //get that data list that the listener's been making. If this is empty or null, make the list a global variable.
+			reader.reader.removeReadListener(srl); //Take the listener off the reader
+			for(String key : allReadData.keySet()){ //This is me separating that gigantic list of all tags into 3 nice categories based on antenna for you; 
+													//the map itself is already Tag : all readings, and now you'll get antenna : tags read too!
 				for(TagReadData tag : allReadData.get(key)){
 					switch(tag.getAntenna()){
 					case 1:
@@ -96,7 +103,7 @@ public class RFIDContinuousReading{
 			}
 			//At this point you should have all the readings for a given antenna in its respective list.
 			//If you want the TagReadData from the three different antennas for a given TagReadData object, then I've written
-			//a helper for you too: 
+			//a helper for you too, which is also javadoced above main.
 			//extractGivenTag(TagReadData tagYouWantDataFor). 
 			Map<String, Double> radianStorage = new HashMap<String, Double>();
 			Map<String, Double> timeStorage = new HashMap<String, Double>();
@@ -108,6 +115,8 @@ public class RFIDContinuousReading{
 				System.err.println("No tags were detected.");
 			}
 			else{
+				//AT THIS POINT THE CODE IS THE EXACT SAME AS IT WAS IN THE TWO ANTENNA CASE. YOU MAY NEED
+				// TO DO YOUR TRIANGLE IMPLEMENTATION HERE. 
 				//Current implementation works for exactly two readings, not more; you need to do averaging/other work in that case.
 				List<String> doubleDetectedTags = new ArrayList<String>();
 				for (TagReadData currentReading : readings){
@@ -168,8 +177,18 @@ public class RFIDContinuousReading{
 	}
 
 
-
-static class SeperateRecordsListener implements ReadListener
+/**
+ * This class is how you implement a readListener. It inherits one abstract method, tagRead(). 
+ * As implemented, it just sticks the tags in a gigantic list that you have to sort through later.
+ * 
+ * A better way to do it is to notice that I've made three global arrays for holding the TagReadData objects for a given channel, and 
+ * in the method where it currently builds up the readTags map, you could directly just call tag.getAntenna() and put the Data in the
+ * right list. If you don't want to do this yourself, I believe I've already implmented it somewhere above in main. (perhaps imperfectly but you'll definitely
+ * get the idea) 
+ * @author DeJuan
+ *
+ */
+static class SeperateRecordsListener implements ReadListener 
 {
 	
   Map<String, List<TagReadData>> readTags = new HashMap<String, List<TagReadData>>();
